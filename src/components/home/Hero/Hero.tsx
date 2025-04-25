11b7e2c81933633ecf6668820c77f9329ef1fbf0
Tajbir23 <studenttajbirislam@gmail.com>
2025-04-25 09:01:05 +0600

    test

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -3,10 +3,8 @@ import { getAllSliders } from "@/app/actions/homeActions";
 import Sliders from "./Sliders";
 
 
-
 const Hero = async() => {
   const {slider} = await getAllSliders()
-  console.log(slider)
   return (
     <div className="mt-5">
       <Sliders slides={slider} />
